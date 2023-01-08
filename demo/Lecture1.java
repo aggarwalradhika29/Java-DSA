baa361aa31d10af0bb8562c962461b2f25eacb2f
AggarwalRadhika29 <singlanorale17@gmail.com>
2023-01-08 20:44:41 +0530

    first lecture file

---
+++ demo/Lecture1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+
+
+public class Lecture1{ 
+    public static void main(String[] args) {
+        
+        Scanner sc= new Scanner(System.in);
+        // System.out.print("Enter a number: ");
+        // int r= sc.nextInt();
+        int r= 32_000_334;
+        System.out.println(r);
+        // a= sc.next
+        sc.close();
+        
+        // int r= 14;
+        // char l= 'r';
+        // float m= 64.4f;
+        // double large= 433847834.45845789;
+        // long lint= 8558745935794792459L;
+        // boolean check = false;f
+
+        // Integer rno= 64;
+        // rno.toString();32
+        
+        // System.out.println((rno));
+
+
+    }
+}
